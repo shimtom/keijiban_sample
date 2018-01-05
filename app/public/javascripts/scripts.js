b7c5e5d357e104596322ad67f7f1e2ce34e66adb
Tomoshige Shimomura <ii00zero1230@gmail.com>
2018-01-06 01:40:54 +0900

    sign up/inの処理を記述.alertの処理を記述

--- app/public/javascripts/scripts.js
+++ app/public/javascripts/scripts.js
@@ -2,20 +2,98 @@ var host = "http://127.0.0.1:3000";
 var user = "admin";
 var board_id = 1;
 
+function signin() {
+  var username = $('#login_field').val();
+  var password = $('#password').val();
+
+  if (username.length === 0 || password.length === 0) {
+    showAlert('username and password can\'t be blank');
+    return;
+  }
+
+  var req = {
+    'username': username,
+    'password': password
+  };
+
+  $.post(host + '/login', req)
+    .done(function (res) {
+      console.log('login response');
+      console.log(res);
+      console.log(e);
+      if (res.username === username && res.status === 'login') {
+        console.log('認証成功');
+        location.replace(location.origin);
+        console.log(location)
+      }
+    })
+    .fail(function (jqXHR) {
+      var res = jqXHR.responseJSON || {};
+      var msg = res.message || 'sign in error';
+      showAlert(msg);
+    });
+
+}
+
+function signup() {
+  var username = $('#inputUsername').val();
+  var displayname = $('#inputDisplayname').val();
+  var password = $('#inputPassword').val();
+
+  if (username.length === 0 || password.length === 0) {
+    showAlert('username and password can\'t be blank');
+    return;
+  }
+
+  console.log('sign up ...');
+
+  var req = {
+    'username': username,
+    'display_name': displayname,
+    'password': password
+  };
+
+  $.post(host + '/users', req)
+    .done(function (res) {
+      console.log(res);
+      console.log('登録成功');
+      location.replace(location.origin);
+    })
+    .fail(function (jqXHR) {
+      var res = jqXHR.responseJSON || {};
+      var msg = res.message || 'sign up error';
+      showAlert(msg);
+    });
+}
+
+function init() {
+  init_alert();
+  $('button#signup').on('click', function () {
+    console.log('signup clicked');
+    signup();
+  });
+
+  $('button#signin').on('click', function () {
+    console.log('sign in clicked');
+    signin();
+  });
+
+}
+
 // コメントを取得し,描画する.
-function render_comments(){
+function render_comments() {
   var comments = $("#comments");
   console.log("rendoer comments");
-  $.getJSON(host + "/api/boards/" + board_id + "/comments", function(res){
+  $.getJSON(host + "/api/boards/" + board_id + "/comments", function (res) {
     console.log("render comments");
     comments.empty();
-    res.forEach(function(value, index) {
+    res.forEach(function (value, index) {
       var created_at = value.created_at;
       var content = value.content;
 
       var header = $("<div>", {
         "class": "panel-heading",
-        "text": (index + 1) + ". 匿名: " +  created_at
+        "text": (index + 1) + ". 匿名: " + created_at
       });
 
       var body = $("<div>", {
@@ -37,8 +115,8 @@ function render_comments(){
 }
 
 // コメントをポストする.
-function post_comment(){
-  $('#comment-form button').on('click', function() {
+function post_comment() {
+  $('#comment-form button').on('click', function () {
     console.log('post comment');
     // comment form の値を取得
     var textarea = $("#comment-form textarea");
@@ -53,12 +131,35 @@ function post_comment(){
       "username": user
     };
     console.log('comment:', comment);
-    $.post(host + "/api/boards/"+ board_id +"/comments", comment,function(res) {
+    $.post(host + "/api/boards/" + board_id + "/comments", comment, function (res) {
       render_comments();
     });
 
   });
 }
 
+function init_alert() {
+  var alert = $('#alert');
+  if (alert) {
+    alert.hide();
+    alert.children('button.close').on('click', function (e) {
+      e.preventDefault();
+      alert.addClass('hidden');
+      alert.hide();
+    });
+  }
+}
+
+function showAlert(text) {
+  var alert = $('#alert');
+  if (alert) {
+    alert.children('.alert-text').text(text);
+    alert.removeClass('hidden');
+    alert.show();
+  }
+}
+
 post_comment();
 render_comments();
+init();
+
