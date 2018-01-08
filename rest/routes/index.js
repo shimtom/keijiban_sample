af69d628ddd2ad9e8aca019bb4ee11f661b309b2
Tomoshige Shimomura <ii00zero1230@gmail.com>
2018-01-09 03:47:38 +0900

    不要なログを削除.サインインユーザーを表示できるように

--- rest/routes/index.js
+++ rest/routes/index.js
@@ -1,14 +1,9 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', function (req, res, next) {
-  console.log('index page');
-  console.log('request session', req.session);
-  console.log('session id', req.sessionID);
-  console.log('request user', req.user);
-
+router.get('/', function (req, res) {
   if (req.user) {
-    res.render('index', {title: '匿名掲示板', status: 'login'});
+    res.render('index', {title: '匿名掲示板', username: req.user.name});
   } else {
     res.render('sign', {alertFlash: req.flash('error')});
   }
