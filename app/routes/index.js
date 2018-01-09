170fdcc2c6d2f2b7fbfabbacc4c3812f543a14a8
Tomoshige Shimomura <ii00zero1230@gmail.com>
2018-01-10 03:52:03 +0900

    不要なコードを削除

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,13 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  if (req.user) {
-    console.log('index');
-    res.render('index', {title: '匿名掲示板'})
-  } else {
-    console.log('sign');
-    res.render('sign', {status: 'logout'});
-  }
+  res.render('index');
 });
 
 module.exports = router;
