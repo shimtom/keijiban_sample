347337cc21024126ccc4e8bcd505d6e64de5643a
Tomoshige Shimomura <ii00zero1230@gmail.com>
2018-01-10 03:46:12 +0900

    不要なモジュールを削除

--- app/app.js
+++ app/app.js
@@ -1,14 +1,12 @@
 // 必要なパッケージの読み込み
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 // 分割したファイルを読み込み
 var index = require('./routes/index');
-
+g
 var app = express();
 
 function main() {
@@ -32,11 +30,9 @@ function main() {
 
   // その他のエラー処理
   app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
     res.status(err.status || 500);
     res.render('error');
   });
